a43a7a01e5267fb6b7feb9cf2b91321bfd1e74d9
Julián Caro Linares <jcarolinares@gmail.com>
2016-02-07 11:37:27 +0100

    Parsing the data

--- feed_parser_esnuc3m.py
+++ feed_parser_esnuc3m.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from pyquery import PyQuery as pq
 from lxml import etree
 import urllib
@@ -8,6 +11,8 @@ import feedparser
 #combinando feedparser para obtener el html de cada entrada, junto con pyquery para filtrar la informacion del html se puede sacar cada dato importante
 
 d = feedparser.parse('http://www.esnuc3m.org/rss.xml')
+
+
 
 '''
 print d['feed']['title']
@@ -24,5 +29,28 @@ print d.entries[0].link #link de la primera entrada
 print d.entries[0].published #fecha de publicacion del ultimo articulo
 '''
 
-for post in d.entries:
-    print post.title + ": " + post.link + "\n"
+#for post in d.entries:
+#    print post.title + ": " + post.link + "\n"
+#    print post.description
+
+print d.entries[0].title
+print d.entries[0].link
+#print d.entries[0].description
+
+
+dparser = pq(d.entries[0].description)
+
+
+
+#Selection of data_events
+date_data=dparser('div[class="field-item even"]')
+date_data=date_data.append("**")
+date_data=date_data.text()
+date_data=date_data.split("**")
+
+print date_data[0]
+
+#for x in date_data:
+#	print x
+
+#<div class="field-item even">
